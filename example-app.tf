6d5be11a2bf5d8e60b08b9fb0f59d082c3891950
Hans Kristian Flaatten <hans.flaatten@evry.com>
2020-03-31 21:44:13 +0200

    Expose the application using Ingress

--- example-app.tf
+++ example-app.tf
@@ -3,4 +3,24 @@ resource "helm_release" "example" {
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "nginx"
   version    = "5.1.12"
+
+  set {
+    name   = "service.type"
+    value = "NodePort"
+  }
+
+  set {
+    name   = "ingress.enabled"
+    value = true
+  }
+
+  set {
+    name   = "ingress.hosts.0.name"
+    value = "example.tietoevry.site"
+  }
+
+  set {
+    name   = "ingress.hosts.0.path"
+    value = "/*"
+  }
 }
