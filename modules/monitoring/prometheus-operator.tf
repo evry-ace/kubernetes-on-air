c30c7f71b54922aea7a221e77d86790a2dd6facb
Hans Kristian Flaatten <hans.flaatten@evry.com>
2020-05-05 21:25:05 +0200

    monitoring: upgrade to latest prometheus-operator

--- modules/monitoring/prometheus-operator.tf
+++ modules/monitoring/prometheus-operator.tf
@@ -30,7 +30,7 @@ resource "helm_release" "prometheus-operator" {
   namespace  = kubernetes_namespace.prometheus.metadata[0].name
   repository = "https://kubernetes-charts.storage.googleapis.com"
   chart      = "prometheus-operator"
-  version    = "8.12.13"
+  version    = "8.13.7"
 
   values = [
     data.template_file.prometheus_operator_config.rendered
